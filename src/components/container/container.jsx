dedc6524ba5c9c7e0a8589677b12a1a2bc837698
Rafiur Rahman <rrfoundary@gmail.com>
2022-04-08 22:28:18 +0600

    added container component

---
+++ src/components/container/container.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Container = styled.div({
+  margin: "0 auto",
+  width: "114rem",
+});
+
+export default Container;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,38 +3,43 @@ import logo from "../../assets/logo.png";
 import user from "../../assets/user.png";
 import * as fa from "react-icons/fa";
 import "./header.scss";
+import Container from "../container/container";
 const Header = () => {
   return (
-    <div className="header">
-      <buton>
-        <i className="fa fa-bars"></i>
-      </buton>
-      <a href="#">
-        <img src={logo} alt="" className="header__logo" />
-      </a>
-      <div>
-        <img src={user} alt="" />
-        <span>Join me</span>
-      </div>
-      <div className="header__input-container">
-        <i className="fa fa-search"></i>
-        <input type="text" placeholder="Search" className="header__input" />
-        <i className="fa fa-bars"></i>
-      </div>
-      <div>
-        <button>
-          <fa.FaHeart />
-        </button>
-        <button>
-          <fa.FaEnvelope />
-        </button>
-        <button>
-          <fa.FaBell />
-        </button>
-        <button>
-          <fa.FaPlus /> Post your ad
-        </button>
-      </div>
+    <div className="header my-3">
+      <Container>
+        <div className="header__content">
+          <button>
+            <i className="fa fa-bars"></i>
+          </button>
+          <a href="#" className="mx-3">
+            <img src={logo} alt="" className="header__logo" />
+          </a>
+          <div className="mx-3 header__profile-container">
+            <img src={user} alt="" className="mr-1" />
+            <span>Join me</span>
+          </div>
+          <div className="header__input-container">
+            <i className="fa fa-search"></i>
+            <input type="text" placeholder="Search" className="header__input" />
+            <i className="fa fa-bars"></i>
+          </div>
+          <div>
+            <button>
+              <fa.FaHeart />
+            </button>
+            <button>
+              <fa.FaEnvelope />
+            </button>
+            <button>
+              <fa.FaBell />
+            </button>
+            <button>
+              <fa.FaPlus /> Post your ad
+            </button>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
