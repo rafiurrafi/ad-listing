870c5d54bfc246a943c721586bed48d7e4362a37
Rafiur Rahman <rrfoundary@gmail.com>
2022-04-15 08:53:41 +0600

    added category component

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
 import "./app.scss";
 import Banner from "./components/banner/banner";
+import Category, {
+  CategoryContent,
+  CategoryImg,
+  CategoryTitle,
+} from "./components/category/category";
+import Container from "./components/container/container";
 import Header from "./components/header/header";
 import Sidebar from "./components/sidebar/sidebar";
+import automobile from "./assets/automobile.png";
+import education from "./assets/education.png";
+import electronics from "./assets/electronics.png";
+import fashion from "./assets/fashion.png";
+import food from "./assets/food.png";
+import furniture from "./assets/furniture.png";
 
 const App = () => {
   const [openSidebar, setOpenSidebar] = useState("none");
@@ -11,6 +23,34 @@ const App = () => {
     <div className={`${openSidebar ? "visible" : ""}  app`}>
       <Header setOpenSidebar={setOpenSidebar} />
       <Banner />
+      <Category>
+        <Container>
+          <CategoryContent>
+            <CategoryImg src={automobile} />
+            <CategoryTitle>Electronics</CategoryTitle>
+          </CategoryContent>
+          <CategoryContent>
+            <CategoryImg src={education} />
+            <CategoryTitle>Electronics</CategoryTitle>
+          </CategoryContent>
+          <CategoryContent>
+            <CategoryImg src={electronics} />
+            <CategoryTitle>Electronics</CategoryTitle>
+          </CategoryContent>
+          <CategoryContent>
+            <CategoryImg src={fashion} />
+            <CategoryTitle>Electronics</CategoryTitle>
+          </CategoryContent>
+          <CategoryContent>
+            <CategoryImg src={food} />
+            <CategoryTitle>Electronics</CategoryTitle>
+          </CategoryContent>
+          <CategoryContent>
+            <CategoryImg src={furniture} />
+            <CategoryTitle>Electronics</CategoryTitle>
+          </CategoryContent>
+        </Container>
+      </Category>
       <Sidebar isOpen={openSidebar === "left"} onClose={setOpenSidebar} />
     </div>
   );
---
+++ src/components/category/category.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+const CategoryContainer = styled.div({
+  padding: "10rem 0",
+  "& > *": {
+    display: "grid",
+    gridTemplateColumns: "repeat(6, 1fr)",
+    gridGap: "5rem",
+    textAlign: "center",
+  },
+});
+const CategoryContentStyle = styled.div({
+  padding: "3rem 0",
+  transition: "all .3s",
+  cursor: "pointer",
+  "&:hover": {
+    boxShadow: ".5rem 1rem 1.5rem rgba(0, 0, 0, .25)",
+  },
+});
+const CategoryImgStyle = styled.img({
+  height: "8rem",
+  width: "8rem",
+  objectFit: "cover",
+  marginBottom: "1rem",
+});
+
+const Category = ({ children }) => {
+  return <CategoryContainer>{children}</CategoryContainer>;
+};
+export const CategoryContent = ({ children }) => {
+  return <CategoryContentStyle>{children}</CategoryContentStyle>;
+};
+export const CategoryImg = ({ src }) => {
+  return <CategoryImgStyle src={src} />;
+};
+
+export const CategoryTitle = ({ children }) => {
+  return <div className="category__title">{children}</div>;
+};
+
+export default Category;
